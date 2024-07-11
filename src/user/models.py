01ce8efe6403213ac40489813c157ea0b084cbb7
MD MONJUR MORSHED <mmt3719@gmail.com>
2024-07-11 23:55:44 +0600

    'uesr_model'

--- src/user/models.py
+++ src/user/models.py
@@ -3,4 +3,14 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    pass
+    uuid = models.UUIDField(max_length=124,auto_created=True,primary_key=True,db_index=True)
+    first_name = models.CharField(max_length=50,null=False)
+    middle_name = models.CharField(max_length=50,null=True)
+    last_name = models.CharField(max_length=50,null=False)
+    avatar = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=100,null=False)
+    phone_number = models.CharField(max_length=100,null=True)
+    
+
+    
+
